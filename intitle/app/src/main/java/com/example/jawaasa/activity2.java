a8eabbac45c5e7d4acac1f651896173933f3b4f7
harajit05 <bwubca22375@brainwareuniversity.ac.in>
2024-09-15 11:07:36 +0530

    first commit

---
+++ intitle/app/src/main/java/com/example/jawaasa/activity2.java
@@ -0,0 +1,26 @@
+package com.example.jawaasa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_2);
+
+    }
+    public void next(View view){
+        Intent i = new Intent(activity2.this,MainActivity.class);
+        startActivity(i);
+    }
+}
